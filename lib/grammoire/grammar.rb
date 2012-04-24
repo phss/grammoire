a528c84460b3eb1d0e4b6b06ce78117fade12178
moonpxi <paulo.schneider@gmail.com>
2012-04-24 23:16:58 +0100

    Adding and evaluation grammar rules

--- lib/grammoire/grammar.rb
+++ lib/grammoire/grammar.rb
@@ -1,7 +1,21 @@
 class Grammar
+
+  def initialize
+    @rules = {}
+  end
   
+  def rule(name, &evaluation)
+    @rules[name] = evaluation
+  end
+
+  def rules_names
+    @rules.keys
+  end
+
   def produce(rule)
-    raise GrammarError.new("Rule '#{rule}' doesn't exist in the grammar.")
+    raise GrammarError.new("Rule '#{rule}' doesn't exist in the grammar.") unless rules_names.include?(rule)
+
+    instance_eval &@rules[rule]
   end
 
 end
--- spec/grammoire/grammar_spec.rb
+++ spec/grammoire/grammar_spec.rb
@@ -7,4 +7,25 @@ describe Grammar do
     lambda { grammar.produce(:no_such_rule) }.should raise_error(GrammarError, "Rule 'no_such_rule' doesn't exist in the grammar.")
   end
 
+  it 'should accept rules with any type as name' do
+    grammar.rule(:symbol)
+    grammar.rule('string')
+    grammar.rule(42)
+    
+    grammar.rules_names.should =~ [:symbol, 'string', 42]
+  end
+
+  it 'should produce terminal symbol of a rule' do
+    grammar.rule(:terminal) { 'terminal' }
+
+    grammar.produce(:terminal).should == 'terminal'
+  end
+
+  it 'should evaluate and substitute symbols when producing a non-terminal rule' do
+    grammar.rule(:non_terminal) { 'non-' + produce(:terminal) }
+    grammar.rule(:terminal) { 'terminal' }
+
+    grammar.produce(:non_terminal).should == 'non-terminal'
+  end
+
 end
