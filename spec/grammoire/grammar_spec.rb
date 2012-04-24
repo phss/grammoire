644e6f0542a866aec1faf29dad4c2a58ebe75505
moonpxi <paulo.schneider@gmail.com>
2012-04-24 17:25:23 +0100

    Fixing typo

--- spec/grammoire/grammar_spec.rb
+++ spec/grammoire/grammar_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Grammar do
   let(:grammar) { Grammar.new }
   
-  it 'should raise an exception when trying to produce output for non existant rule' do
+  it 'should raise an exception when trying to produce output for non existent rule' do
     lambda { grammar.produce(:no_such_rule) }.should raise_error(GrammarError, "Rule 'no_such_rule' doesn't exist in the grammar.")
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,2 @@
 require 'rspec'
-# require File.dirname(__FILE__) + '/../lib/grammoire'
 require File.expand_path(File.dirname(__FILE__) + "/../lib/grammoire")
