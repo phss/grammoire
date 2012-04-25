eeadbc1808ca3cf991abd0235aa21c338ff508e4
moonpxi <paulo.schneider@gmail.com>
2012-04-25 22:01:33 +0100

    Productions evaluate action in a given context

--- lib/grammoire/production.rb
+++ lib/grammoire/production.rb
@@ -1,6 +1,5 @@
 class Production
-  attr_reader :action
-
+  
   def initialize(&action)
     @action = action
   end 
--- spec/grammoire/production_spec.rb
+++ spec/grammoire/production_spec.rb
@@ -1,20 +1,13 @@
 require 'spec_helper'
 
 describe Production do
-  it 'should hold an action block' do
-    action = Proc.new {}
-
-    production = Production.new(&action)
-
-    production.action.should == action
-  end
 
   it 'should evaluate action in the given context' do
     context = mock()
     context.expects(:inside_context).returns("context response")
 
     production = Production.new { inside_context }
-    
+
     production.evaluate(context).should == "context response"
   end
 end
