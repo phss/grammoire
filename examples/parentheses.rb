1ffbbc0848daa75d3403f1c12483ee160640cde4
moonpxi <paulo.schneider@gmail.com>
2012-04-26 17:48:32 +0100

    Sort of example for an alternative way of defining productions for the same rule

--- examples/parentheses.rb
+++ examples/parentheses.rb
@@ -1,9 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + "/../lib/grammoire")
 
+# Repeated rules
 grammar = Grammoire.define do
   rule(:s) { produce(:s) + produce(:s) }
   rule(:s) { "(#{produce(:s)})" }
   rule(:s) { '()' }
 end
 
-10.times { puts grammar.produce(:s) }
+10.times { puts grammar.produce(:s) + "\n\n" }
+
+# Rule with multiple options (doesn't work yet...it will blow up)
+grammar = Grammoire.define do
+  rule :s do
+    production { produce(:s) + produce(:s) }
+    production { "(#{produce(:s)})" }
+    production { '()' }
+  end
+end
+
+10.times { puts grammar.produce(:s) + "\n\n" }
