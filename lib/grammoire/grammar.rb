9d8be4658acdd856e9f8b8a702e0fca54e29d5a7
moonpxi <paulo.schneider@gmail.com>
2012-04-27 17:37:33 +0100

    Removing redundant test

--- lib/grammoire/grammar.rb
+++ lib/grammoire/grammar.rb
@@ -22,16 +22,15 @@ module Grammoire
 
     def produce(rule_name)
       raise GrammarError.new("Rule '#{rule_name}' doesn't exist in the grammar.") unless rules_names.include?(rule_name)
-
       
-      return @chooser.select_from(production_rule_for(rule_name)).evaluate(@context)
+      return @chooser.select_from(production_rules_for(rule_name)).evaluate(@context)
     end
 
    private
 
-    def production_rule_for(name)
+    def production_rules_for(name)
       @rules[name].productions
     end
-    
+
   end
 end
