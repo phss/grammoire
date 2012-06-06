2712477741bff3b59c34b61d04b68af753958f5c
moonpxi <paulo.schneider@gmail.com>
2012-06-06 13:43:56 +0100

    Pending test for reworking Grammar specs

--- spec/grammoire/grammar_spec.rb
+++ spec/grammoire/grammar_spec.rb
@@ -2,31 +2,28 @@ require 'spec_helper'
 
 describe Grammar do
   let(:grammar) { Grammar.new }
-  
-  describe '(validation)' do
-    it 'should raise an exception when trying to evaluate output for non existent rule' do
-      lambda { grammar.evaluate(:no_such_rule) }.should raise_error(GrammarError, "Rule 'no_such_rule' doesn't exist in the grammar or don't have valid pre-conditions.")
-    end
 
-    it 'should raise an exception when evaluating a rule without valid productions' do
-      grammar.rule(:invalid) { pre_condition { false }; produce { 'not valid' } }
+  it 'evaluates existing rule' do
+    pending
+    
+    rules = mock()
+    rule = mock()
+    context = mock()
 
-      lambda { grammar.evaluate(:invalid) }.should raise_error(GrammarError, "Rule 'invalid' doesn't exist in the grammar or don't have valid pre-conditions.")
-    end
+    rules.expects(:for).with(:some_rule).returns(rule)
+    rule.expects(:evaluate).with(context).returns(:expected_output)
+
+    grammar.evaluate(:some_rule).should == :expected_output
   end
+
+  # TODO: replace the acceptance tests below by something more appropriate
+  # for the unit level
 
   describe '(producing)' do
     it 'should evaluate terminal symbol of a rule' do
       grammar.rule(:terminal) { produce { 'terminal' } }
 
       grammar.evaluate(:terminal).should == 'terminal'
-    end
-
-    it 'should evaluate and substitute symbols when producing a non-terminal rule' do
-      grammar.rule(:non_terminal) { produce { 'non-' + eval(:terminal) } }
-      grammar.rule(:terminal) { produce { 'terminal' } }
-
-      grammar.evaluate(:non_terminal).should == 'non-terminal'
     end
 
     it 'should select one of the production rules' do
@@ -71,6 +68,18 @@ describe Grammar do
       grammar.rule(:custom) { produce { custom_method } }
 
       grammar.evaluate(:custom).should == "hello there"
+    end
+  end
+
+  describe '(validation)' do
+    it 'should raise an exception when trying to evaluate output for non existent rule' do
+      lambda { grammar.evaluate(:no_such_rule) }.should raise_error(GrammarError, "Rule 'no_such_rule' doesn't exist in the grammar or don't have valid pre-conditions.")
+    end
+
+    it 'should raise an exception when evaluating a rule without valid productions' do
+      grammar.rule(:invalid) { pre_condition { false }; produce { 'not valid' } }
+
+      lambda { grammar.evaluate(:invalid) }.should raise_error(GrammarError, "Rule 'invalid' doesn't exist in the grammar or don't have valid pre-conditions.")
     end
   end
 
